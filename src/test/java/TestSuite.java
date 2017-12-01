e815ade0218fdf44914c0f2446a9cf6c2442d0a0
Chris Turner <Chrissinjinturner@gmail.com>
2017-12-01 13:04:20 -0800

    Added code for employee classes, and tests as well

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -3,11 +3,11 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        //AdminTest.class,
-        //DoctorTest.class,
+        AdminTest.class,
+        DoctorTest.class,
         EmployeeTest.class,
-        //NurseTest.class,
-        //OrderlyTest.class,
+        NurseTest.class,
+        OrderlyTest.class,
         PatientTest.class
 })
 public class TestSuite {
