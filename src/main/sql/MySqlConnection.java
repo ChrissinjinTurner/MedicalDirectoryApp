4a2510c0536cc64ca99c9ceb2d2ea9dc4873ce10
Chris <chriscarrillo@outlook.com>
2017-12-01 22:20:26 -0800

    MySQL stuff added

---
+++ src/main/sql/MySqlConnection.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnection
+{
+    private static String dbUrl = "jdbc:mysql://107.170.201.221:3306/MedicalDirectory";
+    private static String dbUsername = "chris";
+    private static String dbPassword = "Egr320ChrisCubed";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+    }
+}
