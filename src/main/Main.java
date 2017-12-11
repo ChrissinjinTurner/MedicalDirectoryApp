c55ec8dee5ce0de7af6f19081da13d12ff6629f5
Chris <chriscarrillo@outlook.com>
2017-12-10 21:40:54 -0800

    Fixed the TableView

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,25 +12,6 @@ public class Main extends Application{
     //This is our PrimaryStage (It contains everything)
     private Stage primaryStage;
 
-
-//    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//        String firstName = "Chris";
-//        String lastName = "Carrillo";
-//        String gender = "Male";
-//        String ssn = "123-45-6789";
-//        String dob = "01/23/1997";
-//        double weight = 185.0;
-//        double height = 69.0;
-//        Employee doctor = EmployeeDAO.getEmployee(1);
-//        Employee nurse = EmployeeDAO.getEmployee(2);
-//        String currentCondition = "Healthy";
-//
-//        PatientDAO.addPatient(firstName, lastName, gender, ssn, dob, weight, height, 1, 234, currentCondition);
-//        PatientDAO.editPatient(1, firstName, lastName, gender, "123-456-7890",
-//                dob, weight, height, doctor, nurse, currentCondition);//
-// PatientDAO.deletePatient(1);
-//    }
-
     //Shows the employee operations view inside the root layout.
     public void showEmployeeView() {
         try {
