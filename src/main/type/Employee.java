70e1bd99d0da8766527f8ecfbdc175262a43d84c
Chris <chriscarrillo@outlook.com>
2017-12-10 16:10:15 -0800

    Fixed the Patient and Employee constructors

--- src/main/type/Employee.java
+++ src/main/type/Employee.java
@@ -1,6 +1,8 @@
 package main.type;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 import java.util.Date;
@@ -17,8 +19,16 @@ public class Employee {
     private Date registrationDate;
 
     public Employee() {
-
+        this.id = new SimpleIntegerProperty();
+        this.license = new SimpleIntegerProperty();
+        this.username = new SimpleStringProperty();
+        this.password = new SimpleStringProperty();
+        this.firstName = new SimpleStringProperty();
+        this.lastName = new SimpleStringProperty();
+        this.type = new SimpleIntegerProperty();
+        this.registrationDate = new Date();
     }
+
     public Employee(IntegerProperty id, IntegerProperty license,  StringProperty username, StringProperty password, StringProperty firstName, StringProperty lastName, IntegerProperty type, Date registrationDate) {
         this.id = id;
         this.license = license;
